482c21a0e9c827c12d96ff88b020027ba05b3e2a
Sherif Abdou <sherif_abdou@outlook.com>
2019-10-05 09:07:32 -0700

    Implemented Basic Similarity Check

--- src/DocumentSorter.py
+++ src/DocumentSorter.py
@@ -1,8 +1,19 @@
-from src.DocumentLoader import DocumentLoader
-
+"""
+Sorts a set of Documents using Natural Language Processing
+"""
 class DocumentSorter:
-	def __init__(self, files):
+	threshold = 89.5
+	def __init__(self, files: dict, npl):
 		self.files = files
+		self.npl = npl
 
-	def check_for_similar(self, file):
-		pass
+	def check_for_similar(self, path, file):
+		similar = dict()
+		for other_path, other_file in self.files:
+			if other_path != path:
+				main_doc = self.npl(file)
+				other_doc = self.npl(other_file)
+				similarity = main_doc.similarity(other_doc)
+				print(similarity)
+				if similarity >= DocumentSorter.threshold:
+					similar[other_path] = other_file
