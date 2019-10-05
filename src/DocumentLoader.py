733c26061ac7a4846e4dd6340b39bb5ac0396d9c
Sherif <portal.meteor.master@gmail.com>
2019-10-04 18:38:43 -0700

    Added Text File Searching

--- src/DocumentLoader.py
+++ src/DocumentLoader.py
@@ -1,11 +1,18 @@
 """
 Loads all Text and Word Documents in a Chosen Directory and stores them as Strings
 """
+import os
+import os.path as path
+
 class DocumentLoader():
 	def __init__(self, directory: str):
 		self.directory = directory
 		self.files = dict()
 
-	def loadTextFiles(self):
-		pass
-	
+	def load_text_files(self):
+		files = os.listdir(self.directory)
+		for file in files:
+			parts = file.split(".")
+			ext = parts[len(parts)-1]
+			if ext == "txt":
+				self.add_text_file(path.join(self.directory, file))
