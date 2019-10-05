190ccf0a722453cc808e991f30355e22e007b038
Sherif <portal.meteor.master@gmail.com>
2019-10-04 18:20:20 -0700

    Created requirements file and DocumentLoader base

---
+++ src/DocumentLoader.py
@@ -0,0 +1,11 @@
+"""
+Loads all Text and Word Documents in a Chosen Directory and stores them as Strings
+"""
+class DocumentLoader():
+	def __init__(self, directory: str):
+		self.directory = directory
+		self.files = dict()
+
+	def loadTextFiles(self):
+		pass
+	
