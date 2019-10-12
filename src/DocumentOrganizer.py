1258cfdbf855a9d1949a0cf3a815c1394d8f46a9
Sherif Abdou <sherif_abdou@outlook.com>
2019-10-12 13:43:06 -0700

    Implemented sorting

--- src/DocumentOrganizer.py
+++ src/DocumentOrganizer.py
@@ -4,6 +4,8 @@ Organizes all the files in a given directory based on DocumentSorter outputs
 
 from src.DocumentSorter import DocumentSorter
 from src.DocumentLoader import DocumentLoader
+from src.File import File
+from src.Category import Category
 from shutil import move
 from os import path
 
@@ -15,6 +17,23 @@ class DocumentOrganizer:
         self.document_loader = DocumentLoader(base_dir)
         self.document_loader.load_files()
         self.document_sorter = DocumentSorter(self.document_loader.files, self.nlp)
+        self.categories = []
 
-    def sort_file(self, file):
-        pass
+    def sort_file(self, file: File):
+        if file.sorted:
+            return
+        for category in self.categories:
+            similarity = self.nlp(file.contents).similarity(self.nlp(category.name))
+            if similarity >= DocumentSorter.threshold:
+                category.files.append(file)
+                return
+        similar_files = self.document_sorter.check_for_similar(file.path, file.contents)
+        for other_file in similar_files:
+            if other_file.sorted is False:
+                category = self.document_sorter.category_name_for(file.contents, other_file.contents)
+                file.sorted = True
+                other_file.sorted = True
+                category.files.append(file)
+                category.files.append(other_file)
+                self.categories.append(category)
+                return
--- src/File.py
+++ src/File.py
@@ -4,6 +4,7 @@ class File:
     def __init__(self, path, contents):
         self.path = path
         self.contents = contents
+        self.sorted = False
 
     def __str__(self):
         return "${0}: ${1}".format(self.path, self.contents)
