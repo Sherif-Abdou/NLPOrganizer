c876fdcb2fb87e8e26774dee6f654d7f9d2a7bcc
Sherif Abdou <sherif_abdou@outlook.com>
2019-10-14 18:06:23 -0700

    Formatting + Cache fixes

--- src/DocumentOrganizer.py
+++ src/DocumentOrganizer.py
@@ -11,39 +11,48 @@ from os import path, mkdir
 
 
 class DocumentOrganizer:
-	def __init__(self, base_dir: str, nlp):
-		self.base_dir = base_dir
-		self.nlp = nlp
-		self.document_loader = DocumentLoader(base_dir)
-		self.document_loader.load_files()
-		self.document_sorter = DocumentSorter(self.document_loader.files, self.nlp)
-		self.categories = []
+    def __init__(self, base_dir: str, nlp):
+        self.base_dir = base_dir
+        self.nlp = nlp
+        self.document_loader = DocumentLoader(base_dir)
+        self.document_loader.load_files()
+        self.document_sorter = DocumentSorter(
+            self.document_loader.files, self.nlp)
+        self.categories = []
 
-	def sort_file(self, file: File):
-		if file.sorted:
-			return
-		for category in self.categories:
-			similarity = 0
-			for category_file in category.files:
-				similarity += (self.nlp(file.contents).similarity(self.nlp(category_file.contents)))/len(category.files)
-			if similarity >= DocumentSorter.threshold:
-				category.files.append(file)
-				file.sorted = True
-				return
-		similar_files = self.document_sorter.check_for_similar(file.path, file.contents)
-		for other_file in similar_files:
-			if other_file.sorted is False:
-				category = self.document_sorter.category_name_for(file.contents, other_file.contents)
-				file.sorted = True
-				other_file.sorted = True
-				category.files.append(file)
-				category.files.append(other_file)
-				self.categories.append(category)
-				return
+    def sort_file(self, file: File):
+        if file.sorted:
+            return
+        for category in self.categories:
+            similarity = 0
+            for category_file in category.files:
+                file_doc_no_stop = self.nlp(
+                    ' '.join([str(t) for t in self.nlp(file.contents) if not t.is_stop]))
+                other_doc_no_stop = self.nlp(
+                    ' '.join([str(t) for t in self.nlp(category_file.contents) if not t.is_stop]))
+                similarity += (file_doc_no_stop.similarity(other_doc_no_stop)
+                               )/len(category.files)
+            if similarity >= DocumentSorter.threshold:
+                category.files.append(file)
+                file.sorted = True
+                return
+        similar_files = self.document_sorter.check_for_similar(
+            file.path, file.contents)
+        for other_file in similar_files:
+            if other_file.sorted is False:
+                category = self.document_sorter.category_name_for(
+                    file.contents, other_file.contents)
+                file.sorted = True
+                other_file.sorted = True
+                category.files.append(file)
+                category.files.append(other_file)
+                self.categories.append(category)
+                return
 
-	def move_files(self):
-		for category in self.categories:
-			if not path.exists(path.join(self.base_dir, category.name)):
-				mkdir(path.join(self.base_dir, category.name))
-			for file in category.files:
-				move(file.path, path.join(self.base_dir, category.name, path.basename(file.path)))
+    def move_files(self):
+        for category in self.categories:
+            if not path.exists(path.join(self.base_dir, category.name)):
+                mkdir(path.join(self.base_dir, category.name))
+            for file in category.files:
+                move(file.path, path.join(self.base_dir,
+                                          category.name, path.basename(file.path)))
--- src/DocumentSorter.py
+++ src/DocumentSorter.py
@@ -8,95 +8,95 @@ import json
 from os import path
 from os import mkdir
 
+
 class DocumentSorter:
-	# The threshold for when two files are considered similar
-	threshold = 0.895
+    # The threshold for when two files are considered similar
+    threshold = 0.85
 
-	def __init__(self, files, nlp):
-		self.files = files
-		self.nlp = nlp
-		self.cache_path = "/tmp/cache/npl/cache"
-		self.cache = dict()
-		self.loadcache()
+    def __init__(self, files, nlp):
+        self.files = files
+        self.nlp = nlp
+        self.cache_path = "/tmp/cache/npl/cache"
+        self.cache_folder = "/tmp/cache/npl"
+        self.cache = dict()
+        self.loadcache()
 
-	def loadcache(self):
-		if not path.exists(self.cache_path):
-			mkdir(path.dirname(self.cache_path))
-		else:
-			with open(self.cache_path, "r") as file:
-				self.cache = json.load(file)
+    def loadcache(self):
+        if not path.exists(self.cache_folder):
+            mkdir(self.cache_folder)
+        else:
+            with open(self.cache_path, "r") as file:
+                self.cache = json.load(file)
 
-	def savecache(self):
-		with open(self.cache_path, "w") as file:
-			json.dump(self.cache, file)
+    def savecache(self):
+        with open(self.cache_path, "w") as file:
+            json.dump(self.cache, file)
 
-	# Determines what files are similar to a given file
-	def check_for_similar(self, path: str, file: str):
-		similar = []
-		for other_rawfile in self.files:
-			other_path = other_rawfile.path
-			other_file = other_rawfile.contents
-			if other_path != path:
-				# Loads each file into Spacy's Natural Language Processing
-				main_doc = self.nlp(file)
-				other_doc = self.nlp(other_file)
-				# Removes useless stop words from each file
-				main_doc_no_stop = self.nlp(
-					' '.join([str(t) for t in main_doc if not t.is_stop]))
-				other_doc_no_stop = self.nlp(
-					' '.join([str(t) for t in other_doc if not t.is_stop]))
-				# Compares the two files' similarity
-				similarity = main_doc_no_stop.similarity(other_doc_no_stop)
-				print(similarity)
-				if similarity >= DocumentSorter.threshold:
-					similar.append(other_rawfile)
-		return similar
+    # Determines what files are similar to a given file
+    def check_for_similar(self, path: str, file: str):
+        similar = []
+        for other_rawfile in self.files:
+            other_path = other_rawfile.path
+            other_file = other_rawfile.contents
+            if other_path != path:
+                # Loads each file into Spacy's Natural Language Processing
+                main_doc = self.nlp(file)
+                other_doc = self.nlp(other_file)
+                # Removes useless stop words from each file
+                main_doc_no_stop = self.nlp(
+                    ' '.join([str(t) for t in main_doc if not t.is_stop]))
+                other_doc_no_stop = self.nlp(
+                    ' '.join([str(t) for t in other_doc if not t.is_stop]))
+                # Compares the two files' similarity
+                similarity = main_doc_no_stop.similarity(other_doc_no_stop)
+                if similarity >= DocumentSorter.threshold:
+                    similar.append(other_rawfile)
+        return similar
 
-	# Finds the i most similar words to a given word
-	def most_similar(self, word, i):
-		if word in self.cache:
-			return self.cache[word]
-		lexeme = self.nlp.vocab[word]
-		queries = [w for w in lexeme.vocab if w.is_lower ==
-				   lexeme.is_lower and w.prob >= -15]
-		by_similarity = sorted(
-			queries, key=lambda w: lexeme.similarity(w), reverse=True)
-		output = [w.lower_ for w in by_similarity[:i]]
-		self.cache[word] = output
-		return output
+    # Finds the i most similar words to a given word
+    def most_similar(self, word, i):
+        if word in self.cache:
+            return self.cache[word]
+        lexeme = self.nlp.vocab[word]
+        queries = [w for w in lexeme.vocab if w.is_lower ==
+                   lexeme.is_lower and w.prob >= -15]
+        by_similarity = sorted(
+            queries, key=lambda w: lexeme.similarity(w), reverse=True)
+        output = [w.lower_ for w in by_similarity[:i]]
+        self.cache[word] = output
+        return output
 
-	# Determines a category name
-	def category_name_for(self, file1, file2):
-		# Finds the 5 most common nouns in each document
-		file1_nouns = [token.text for token in self.nlp(file1) if
-					   token.is_stop != True and token.is_punct != True and token.pos_ == "NOUN"]
-		file1_counter = Counter(file1_nouns)
-		file2_nouns = [token.text for token in self.nlp(file2) if
-					   token.is_stop != True and token.is_punct != True and token.pos_ == "NOUN"]
-		file2_counter = Counter(file2_nouns)
-		file1_top = file1_counter.most_common(5)
-		file2_top = file2_counter.most_common(5)
+    # Determines a category name
+    def category_name_for(self, file1, file2):
+        # Finds the 5 most common nouns in each document
+        file1_nouns = [token.text for token in self.nlp(file1) if
+                       token.is_stop != True and token.is_punct != True and token.pos_ == "NOUN"]
+        file1_counter = Counter(file1_nouns)
+        file2_nouns = [token.text for token in self.nlp(file2) if
+                       token.is_stop != True and token.is_punct != True and token.pos_ == "NOUN"]
+        file2_counter = Counter(file2_nouns)
+        file1_top = file1_counter.most_common(5)
+        file2_top = file2_counter.most_common(5)
 
-		# Scores the similar words of each noun based on number of appearances and similarity to original noun
-		scores = dict()
-		for tops in [file1_top, file2_top]:
-			i = 1
-			for top, _ in tops:
-				for similar in self.most_similar(top, 50):
-					if similar in scores:
-						scores[similar] += 1 / i
-					else:
-						scores[similar] = 1 / i
-				i += 0.1
+        # Scores the similar words of each noun based on number of appearances and similarity to original noun
+        scores = dict()
+        for tops in [file1_top, file2_top]:
+            i = 1
+            for top, _ in tops:
+                for similar in self.most_similar(top, 50):
+                    if similar in scores:
+                        scores[similar] += 1 / i
+                    else:
+                        scores[similar] = 1 / i
+                i += 0.1
 
-		# Looks through the scores to find the highest scoring word
-		curr_word = ""
-		curr_score = float("-inf")
-		for word, score in scores.items():
-			print(word)
-			if score > curr_score:
-				curr_word = word
-				curr_score = score
-		self.savecache()
-		# Outputs the highest scoring word
-		return Category(curr_word)
+        # Looks through the scores to find the highest scoring word
+        curr_word = ""
+        curr_score = float("-inf")
+        for word, score in scores.items():
+            if score > curr_score:
+                curr_word = word
+                curr_score = score
+        self.savecache()
+        # Outputs the highest scoring word
+        return Category(curr_word)
