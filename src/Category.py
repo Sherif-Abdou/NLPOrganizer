ba6e350d1fac696c68c1b30f5b1e4eb0ce1d7cba
Sherif Abdou <sherif_abdou@outlook.com>
2019-10-27 17:39:56 -0700

    Added option to time sorting
    To collect data on performance

--- src/Category.py
+++ src/Category.py
@@ -9,6 +9,7 @@ class Category:
     def __init__(self, name: str):
         self.name = name
         self.files = []
+
     def __str__(self):
         files = " : ".join([basename(file.path) for file in self.files])
         return files
